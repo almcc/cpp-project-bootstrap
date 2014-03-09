8b24863a0d101be4dbbc3444e0f1c126f154c66c
Alastair McClelland <alastair.mcclelland@gmail.com>
2014-03-09 14:23:02 +0000

    Dir structure matching namespace

---
+++ source/src/common/App/Configuration/ArgumentParser.h
@@ -0,0 +1,45 @@
+#ifndef ARGUMENT_PARSER_H
+#define ARGUMENT_PARSER_H
+
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <string>
+#include <set>
+
+using namespace std;
+
+namespace App
+{
+  namespace Configuration
+  {
+    class ArgumentParser
+    {
+      friend class ArgumentParserTest;
+
+      public:
+        ArgumentParser();
+        ~ArgumentParser();
+
+        bool parse(int argc, char *argv[]);
+        bool defineAlias(string alias, string actual);
+
+        bool isFlag(string flag);
+        bool isOption(string option);
+        string getOption(string option);
+
+      private:
+
+        bool parseArg(string arg);
+        bool setAlias(string alias);
+        bool setFlag(string flag);
+        bool setOption(string optionValue);
+
+        set<string> flags;
+        map<string, string> aliases;
+        map<string, string> options;
+    };
+  }
+}
+
+#endif
